097dbeb9921f6187dd73aaf32a7eeb5d3dc53355
ansariarifdev <ansariarif.dev@gmail.com>
2026-01-15 18:53:36 +0530

    backend+db connection successful

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { type Request, type Response } from "express";
+import { prismaClient } from "./lib/client";
+
+const app = express();
+
+app.get("/", async (req: Request, res: Response) => {
+    const data = await prismaClient.user.findMany();
+    res.json({
+        data
+    })
+})
+
+app.post("/", async (req, res) => {
+    const data = await prismaClient.user.create({
+        data: {
+            email: "ansariarif@gmail.com", 
+            password: "ansariarif"
+        }
+    })
+    res.json({
+        data
+    })
+})
+
+app.listen(3000);
